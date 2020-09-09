bcd0ce6f39cbb4b0dee1fa397873772ad842598e
Doug Gschwind <doug.gschwind@gmail.com>
2020-09-09 15:25:36 -0700

    Introduce another unit test method asserting that the Euchre cards in one players hand cannot be found in any of the other players hands

--- src/test/java/org/douggschwind/games/cardgames/euchre/ClassicEuchreTest.java
+++ src/test/java/org/douggschwind/games/cardgames/euchre/ClassicEuchreTest.java
@@ -1,5 +1,6 @@
 package org.douggschwind.games.cardgames.euchre;
 
+import org.douggschwind.games.cardgames.common.Card;
 import org.douggschwind.games.cardgames.common.Player;
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,6 +22,7 @@ public class ClassicEuchreTest {
 
         underTest.newHand();
         underTest.dealCardsToPlayers();
+        Assert.assertNotNull(underTest.getInitialTrumpBiddingCardFromKitty());
 
         Assert.assertEquals(5, player1.getHand().size());
         Assert.assertEquals(5, player2.getHand().size());
@@ -33,5 +35,49 @@ public class ClassicEuchreTest {
         Assert.assertTrue(player2.getHand().isEmpty());
         Assert.assertTrue(player3.getHand().isEmpty());
         Assert.assertTrue(player4.getHand().isEmpty());
+    }
+
+    @Test
+    public void testPlayersAllGetDistinctCards() {
+        ClassicEuchre underTest = new ClassicEuchre();
+
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
+
+        underTest.addPlayer(player1);
+        underTest.addPlayer(player2);
+        underTest.addPlayer(player3);
+        underTest.addPlayer(player4);
+
+        underTest.newHand();
+        underTest.dealCardsToPlayers();
+
+        // Lets make sure that the cards in any one Player's hand are NOT
+        // found in any of the other Player's hands.
+        for (Card player1Card : player1.getHand()) {
+            Assert.assertFalse(player2.getHand().contains(player1Card));
+            Assert.assertFalse(player3.getHand().contains(player1Card));
+            Assert.assertFalse(player4.getHand().contains(player1Card));
+        }
+
+        for (Card player2Card : player2.getHand()) {
+            Assert.assertFalse(player1.getHand().contains(player2Card));
+            Assert.assertFalse(player3.getHand().contains(player2Card));
+            Assert.assertFalse(player4.getHand().contains(player2Card));
+        }
+
+        for (Card player3Card : player3.getHand()) {
+            Assert.assertFalse(player1.getHand().contains(player3Card));
+            Assert.assertFalse(player2.getHand().contains(player3Card));
+            Assert.assertFalse(player4.getHand().contains(player3Card));
+        }
+
+        for (Card player4Card : player4.getHand()) {
+            Assert.assertFalse(player1.getHand().contains(player4Card));
+            Assert.assertFalse(player2.getHand().contains(player4Card));
+            Assert.assertFalse(player3.getHand().contains(player4Card));
+        }
     }
 }
