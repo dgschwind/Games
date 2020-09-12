ce501ce5c0a51a38803e4bf36fd7f62b15e6b1df
Doug Gschwind <doug.gschwind@gmail.com>
2020-09-12 04:04:53 -0700

    Introduce additional instance variable to make dealing of cards simplified. Renamed cards instance variable to deckConfiguration for clarity

--- src/main/java/org/douggschwind/games/common/DeckOfCards.java
+++ src/main/java/org/douggschwind/games/common/DeckOfCards.java
@@ -14,72 +14,43 @@ import java.util.Random;
  */
 public class DeckOfCards <C extends AbstractCard> {
 	
-	private final List<C> cards = new ArrayList<>();
+	private final List<C> deckConfiguration = new ArrayList<>();
+	private final List<C> cardsRemainingInDeck = new ArrayList<>();
 	private final Map<C, Boolean> dealtCards = new HashMap<>();
 	private Random random;
 	
 	public void addCard(C toAdd) {
 		if (toAdd != null) {
-			cards.add(toAdd);
-			dealtCards.put(toAdd, false);
+			deckConfiguration.add(toAdd);
 		}
 	}
 
 	public int size() {
-		return cards.size();
+		return deckConfiguration.size();
 	}
 	
 	public void shuffle() {
 		random = new Random();
-		dealtCards.keySet().stream().forEach(key -> dealtCards.put(key, false));
+		cardsRemainingInDeck.clear();
+		cardsRemainingInDeck.addAll(deckConfiguration);
+		dealtCards.clear();
 	}
 	
 	protected final boolean hasCardBeenDealt(AbstractCard card) {
 		return (dealtCards.get(card) == true);
 	}
 	
-	private C getRandomCardNotYetDealt() {
-		final int randomlySelectedCardIndex = random.nextInt(cards.size());
-		C randomlySelectedCard = cards.get(randomlySelectedCardIndex);
-		if (!hasCardBeenDealt(randomlySelectedCard)) {
-			return randomlySelectedCard;
-		}
-		
-		// The randomly selected card has already been dealt. Lets just
-		// pick its closest neighbor that has not already been dealt.
-		
-		int indexDistanceFromOrigin = 1;
-		while (true) {
-			int candidateSelectedCardIndex = randomlySelectedCardIndex + indexDistanceFromOrigin;
-			if (candidateSelectedCardIndex < cards.size()) {
-				randomlySelectedCard = cards.get(candidateSelectedCardIndex);
-				if (!hasCardBeenDealt(randomlySelectedCard)) {
-					return randomlySelectedCard;
-				}
-			}
-			
-			candidateSelectedCardIndex = randomlySelectedCardIndex - indexDistanceFromOrigin;
-			if (candidateSelectedCardIndex >= 0) {
-				randomlySelectedCard = cards.get(candidateSelectedCardIndex);
-				if (!hasCardBeenDealt(randomlySelectedCard)) {
-					return randomlySelectedCard;
-				}
-			}
-			
-			indexDistanceFromOrigin++;
-		}
-	}
-
 	protected final boolean haveAllCardsBeenDealt() {
-		return cards.stream().allMatch(card -> hasCardBeenDealt(card));
+		return cardsRemainingInDeck.isEmpty();
 	}
 	
 	public C dealCard() {
 		if (haveAllCardsBeenDealt()) {
 			throw new IllegalStateException("All cards have already been dealt!");
 		}
-		C randomlySelectedCard = getRandomCardNotYetDealt();
-		dealtCards.put(randomlySelectedCard, true);
-		return randomlySelectedCard;
+		final int randomlySelectedCardIndex = random.nextInt(cardsRemainingInDeck.size());
+		C result = cardsRemainingInDeck.remove(randomlySelectedCardIndex);
+		dealtCards.put(result, true);
+		return result;
 	}
 }
