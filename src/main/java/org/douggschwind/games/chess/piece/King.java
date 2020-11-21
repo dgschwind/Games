b8e22761ddc6bc085b78088ffa743e7cd73ce9c4
Doug Gschwind <doug.gschwind@gmail.com>
2020-11-20 22:01:31 -0700

    Implement canMoveTo() and moveTo() for King

--- src/main/java/org/douggschwind/games/chess/piece/King.java
+++ src/main/java/org/douggschwind/games/chess/piece/King.java
@@ -5,6 +5,7 @@ import org.douggschwind.games.chess.Square;
 
 /**
  * Represents the King piece in the game of Chess, for a given Player.
+ * TODO: Support for castling.
  * @author Doug Gschwind
  */
 public class King extends ChessPiece {
@@ -18,12 +19,38 @@ public class King extends ChessPiece {
         return true;
     }
 
+    /**
+     * Kings move one square in any direction, so long as that square is not attacked by an enemy piece.
+     * Additionally, kings are able to make a special move, know as castling.
+     * @param chessBoard Must be non-null.
+     * @param from The piece's current BoardPosition.
+     * @param to The piece's proposed destination square.
+     * @return
+     */
     @Override
     public boolean canMoveTo(ChessBoard chessBoard, Square from, Square to) {
-        return false; //TODO
+        if (!basicCanMoveTo(from, to)) {
+            return false;
+        }
+
+        if (to.isOccupied() && from.isOccupiedByMyOpponent(to)) {
+            return false;
+        }
+
+        final int numRowsMovement = Math.abs(from.getRow().getId() - to.getRow().getId());
+        final int numColumnsMovement = Math.abs(from.getColumn().getId() - to.getColumn().getId());
+
+        if (numRowsMovement == 0) {
+            return numColumnsMovement == 1;
+        } else if (numColumnsMovement == 0) {
+            return numRowsMovement == 1;
+        } else {
+            return ((numRowsMovement == 1) && (numColumnsMovement == 1));
+        }
     }
 
     @Override
     public void moveTo(ChessBoard chessBoard, Square from, Square to) {
+        basicMove(from, to);
     }
 }
