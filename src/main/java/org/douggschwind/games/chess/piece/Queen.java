f6c22e006941d7a4e866df492d2ecf6e2e54e105
Doug Gschwind <doug.gschwind@gmail.com>
2020-11-20 21:52:17 -0700

    Implement canMoveTo() and moveTo() for Queen

--- src/main/java/org/douggschwind/games/chess/piece/Queen.java
+++ src/main/java/org/douggschwind/games/chess/piece/Queen.java
@@ -18,12 +18,36 @@ public class Queen extends ChessPiece {
         return true;
     }
 
+    /**
+     * Queens move diagonally, horizontally, or vertically any number of squares. They are unable to jump over pieces.
+     * @param chessBoard Must be non-null.
+     * @param from The piece's current BoardPosition.
+     * @param to The piece's proposed destination square.
+     * @return
+     */
     @Override
     public boolean canMoveTo(ChessBoard chessBoard, Square from, Square to) {
-        return false; //TODO
+        if (!basicCanMoveTo(from, to)) {
+            return false;
+        }
+
+        // Number of rows of movement must equal number of columns to move diagonally.
+        final int numRowsMovement = Math.abs(from.getRow().getId() - to.getRow().getId());
+        final int numColumnsMovement = Math.abs(from.getColumn().getId() - to.getColumn().getId());
+        boolean isMovingDiagonally = (numRowsMovement == numColumnsMovement);
+
+        boolean isMovingHorizontally = (from.getRow() == to.getRow());
+        boolean isMovingVertically = (from.getColumn() == to.getColumn());
+
+        if (!isMovingHorizontally && !isMovingVertically && !isMovingDiagonally) {
+            return false;
+        }
+
+        return chessBoard.isPathClear(from, to);
     }
 
     @Override
     public void moveTo(ChessBoard chessBoard, Square from, Square to) {
+        basicMove(from, to);
     }
 }
